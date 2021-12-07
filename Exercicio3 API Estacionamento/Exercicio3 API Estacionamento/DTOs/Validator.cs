51065bedec5c9fc25f7b2ff93a6357162aaf7842
Shippozudo <86241457+Shippozudo@users.noreply.github.com>
2021-12-07 15:07:12 -0300

    Delete Exercicio3 API Estacionamento directory

--- Exercicio3 API Estacionamento/Exercicio3 API Estacionamento/DTOs/Validator.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Exercicio3_API_Estacionamento.DTOs
-{
-    public abstract class Validator
-    {
-        public bool Valido { get; protected set; }
-        public abstract void Validar();
-    }
-}
--- Exercicio3 API Estacionamento/Exercicio3 API Estacionamento/Enumeradores/EtipoVeiculo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Exercicio3_API_Estacionamento.Enumeradores
-{
-    public enum EtipoVeiculo
-    {
-        Carro = 0,
-        Moto = 1
-    }
-}
